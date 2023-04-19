cade1e6de4890e653e7436ea8a44aded18626cbe
DinGis9 <oleg777gorbachov@gmail.com>
2023-04-19 17:36:09 +0300

    env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+import { EnvI } from 'src/types';
+
+export const environment: EnvI = {
+  RapidAPI_Host: 'youtube-v31.p.rapidapi.com',
+  RapidAPI_Key: '8d69778763msh2630db8f6feab33p1d33f3jsn2adbfb72df88',
+};
